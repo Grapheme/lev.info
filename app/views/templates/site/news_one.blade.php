e7bdc0b45db80ed741836850749fb9dc737bb192
KostyaD <thedamaxstudio@gmail.com>
2015-03-04 20:12:12 +0300

    fix

--- app/views/templates/site/news_one.blade.php
+++ app/views/templates/site/news_one.blade.php
@@ -16,7 +16,7 @@ $seo = $new->seo;
 @section('style')
     <meta property="og:title" content="{{ $new->name }}">
     <meta property="og:description" content="{{ $new->preview }}">
-    <meta property="og:image" content="{{ $new->image }}">
+    <meta property="og:image" content="{{ $new->image->full() }}">
 @stop
 
 
